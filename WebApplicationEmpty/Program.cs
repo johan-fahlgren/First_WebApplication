a84a100a4f31bb1b2bdb71437873719f63777898
Johan Fahlgren <johan.fahlgren@utb.ecutbildning.se>
2022-03-09 21:56:41 +0100

    Add project files.

---
+++ WebApplicationEmpty/Program.cs
@@ -0,0 +1,37 @@
+//PHASE 1 - configuring of webapp
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
+
+
+
+
+
+//PHASE 2 - Middleware pipelining (Configure the HTTP request pipeline).
+
+var app = builder.Build();
+
+app.UseRouting();
+/*
+app.MapGet("/", () => "Hello World!");
+*/
+
+// Exercise Mapping
+//app.Map("/welcome", () => "Hello and welcome");
+//app.MapGet("/blogPost/{date}", (DateTime date) => DbService.getBlog(date));
+
+app.MapRazorPages();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=snippet}/{action=Welcome}"); // = default values.
+
+
+
+
+
+
+//PHASE 3  - Server started
+app.Run();
